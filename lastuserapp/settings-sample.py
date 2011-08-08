278d81724ddee5d3685c0b0f4b944832771df151
Aditya <aditya@netroy.in>
2011-08-09 02:57:15 +0530

    we should keep some gap between sections.. makes it readable

--- lastuserapp/settings-sample.py
+++ lastuserapp/settings-sample.py
@@ -2,18 +2,25 @@
 
 #: The title of this site
 SITE_TITLE='LastUser'
+
 #: Support contact email
 SITE_SUPPORT_EMAIL = 'test@example.com'
+
 #: TypeKit code for fonts
 TYPEKIT_CODE=''
+
 #: Google Analytics code UA-XXXXXX-X
 GA_CODE=''
+
 #: Database backend
 SQLALCHEMY_DATABASE_URI = 'sqlite:///test.db'
+
 #: Secret key
 SECRET_KEY = 'make this something random'
+
 #: Timezone
 TIMEZONE = 'Asia/Calcutta'
+
 #: Mail settings
 #: MAIL_FAIL_SILENTLY : default True
 #: MAIL_SERVER : default 'localhost'
@@ -26,12 +33,16 @@ TIMEZONE = 'Asia/Calcutta'
 MAIL_FAIL_SILENTLY = False
 MAIL_SERVER = 'localhost'
 DEFAULT_MAIL_SENDER = ('LastUser', 'test@example.com')
+
 #: Logging: recipients of error emails
 ADMINS=[]
+
 #: Log file
 LOGFILE='error.log'
+
 #: Use SSL for some URLs
 USE_SSL=False
+
 #: Twitter integration
 OAUTH_TWITTER_KEY=''
 OAUTH_TWITTER_SECRET=''
@@ -45,9 +56,11 @@ RECAPTCHA_USE_SSL=USE_SSL
 RECAPTCHA_PUBLIC_KEY=''
 RECAPTCHA_PRIVATE_KEY=''
 RECAPTCHA_OPTIONS=''
+
 #: SMS gateways
 SMS_SMSGUPSHUP_MASK=''
 SMS_SMSGUPSHUP_USER=''
 SMS_SMSGUPSHUP_PASS=''
+
 #: Messages (in markdown)
 MESSAGE_FOOTER='Copyright &copy; HasGeek. Powered by LastUser, open source software from HasGeek.'
