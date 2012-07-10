8f986765570ce15a97330797a2843b1aa14f6291
Nigel Babu <nigel@hasgeek.com>
2012-07-10 14:00:50 +0530

    Moved assets into __init__.py

--- lastuserapp/__init__.py
+++ lastuserapp/__init__.py
@@ -34,11 +34,17 @@ configureapp(app, 'LASTUSER_ENV')
 app.register_blueprint(baseframe)
 assets = Environment(app)
 
+js = Bundle('js/libs/jquery-1.5.1.min.js',
+            'js/libs/jquery.form.js',
+            'js/scripts.js',
+            filters='jsmin', output='js/packed.js')
+
+assets.register('js_all', js)
+
 for msg in __MESSAGES:
     app.config[msg] = Markup(markdown(app.config.get(msg, '')))
 
 
-import lastuserapp.assets
 import lastuserapp.mailclient
 import lastuserapp.models
 import lastuserapp.forms
