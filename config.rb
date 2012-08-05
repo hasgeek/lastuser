86c759b849658f3223de1e89c74a60445aaddceb
Kiran Jonnalagadda <kiran@hasgeek.com>
2012-08-05 13:40:06 +0530

    Moved sass folder; styles for markdown in Funnel.

--- config.rb
+++ config.rb
@@ -2,9 +2,9 @@
 # Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "lastuserapp/static/css"
-sass_dir = "sass"
+sass_dir = "lastuserapp/static/sass"
 images_dir = "lastuserapp/static/img"
 javascripts_dir = "lastuserapp/static/js"
 line_comments = false
 # To enable relative paths to assets via compass helper functions. Uncomment:
-# relative_assets = true
+relative_assets = true
