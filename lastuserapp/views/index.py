43b2dcd61cc896c0cb20fe855f801e6a42a8820f
Kiran Jonnalagadda <kiran@hasgeek.com>
2013-01-21 15:26:22 +0530

    Move robots.txt to a plain text file; add more.

--- lastuserapp/views/index.py
+++ lastuserapp/views/index.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from flask import render_template, Response
+from flask import render_template
 
 from lastuserapp import app
 
@@ -8,11 +8,3 @@ from lastuserapp import app
 @app.route('/')
 def index():
     return render_template('index.html')
-
-
-@app.route('/robots.txt')
-def robots():
-    return Response("Disallow: /auth/*\n"
-                    "Disallow: /token/*\n"
-                    "",
-                    content_type='text/plain; charset=utf-8')
