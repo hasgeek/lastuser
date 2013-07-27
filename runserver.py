5a9fd92c33c57cd90d6cd2c615f185f51298d8d5
Kiran Jonnalagadda <kiran@hasgeek.com>
2013-07-27 17:18:19 +0530

    Don't autocreate db tables.

--- runserver.py
+++ runserver.py
@@ -5,8 +5,6 @@ reload(sys)
 sys.setdefaultencoding('utf-8')
 
 from lastuserapp import app, init_for
-from lastuser_core.models import db
 
 init_for('dev')
-db.create_all()
 app.run('0.0.0.0', port=7000, debug=True)
