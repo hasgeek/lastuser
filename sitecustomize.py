74a6e1cdaf25c411f3b6e4c9f9049a6ef04591d1
Kiran Jonnalagadda <kiran@hasgeek.com>
2013-10-09 09:34:08 +0530

    Fix hasattr spelling.

--- sitecustomize.py
+++ sitecustomize.py
@@ -1,5 +1,5 @@
 # Required to make OpenID work with Wordpress (first instance where it came up)
 import sys
-if not hasttr(sys, 'setdefaultencoding'):
+if not hasattr(sys, 'setdefaultencoding'):
     reload(sys)
 sys.setdefaultencoding("utf-8")
