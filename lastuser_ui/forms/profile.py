e5813c17467b890c57fd1b945de7941b44edfbde
Kiran Jonnalagadda <kiran@hasgeek.com>
2015-02-10 13:42:51 +0530

    Handle errors when Twilio verification fails

--- lastuser_ui/forms/profile.py
+++ lastuser_ui/forms/profile.py
@@ -36,39 +36,43 @@ class NewEmailAddressForm(Form):
 
 class NewPhoneForm(Form):
     phone = wtforms.TextField(__("Phone number"), default='+91',
-        validators=[
-            wtforms.validators.Required(),
-            wtforms.validators.Length(min=1, max=16, message=__("This is too long to be a valid phone number"))],
-        description=__("Mobile numbers only"))
+        validators=[wtforms.validators.Required()],
+        description=__("In international calling format starting with '+' and country code. Mobile numbers only at this time"))
     type = wtforms.RadioField(__("Type"), coerce=nullunicode, validators=[wtforms.validators.Optional()], choices=[
         (__(u"Mobile"), __(u"Mobile")),
-        (__(u"Home"), __(u"Home")),
-        (__(u"Work"), __(u"Work")),
+        # (__(u"Home"), __(u"Home")),
+        # (__(u"Work"), __(u"Work")),
         (__(u"Other"), __(u"Other"))])
 
     def validate_phone(self, field):
-        existing = UserPhone.get(phone=field.data)
+        # Step 1: Remove punctuation in number
+        number = strip_phone(field.data)
+        # Step 2: Check length
+        if len(number) > 16:
+            raise wtforms.ValidationError(_("This is too long to be a valid phone number"))
+        # Step 3: Validate number format
+        if not valid_phone(number):
+            raise wtforms.ValidationError(_("Invalid phone number (must be in international format with a leading + symbol)"))
+        # Step 4: Check if Indian number (startswith('+91'))
+        if number.startswith('+91') and len(number) != 13:
+            raise wtforms.ValidationError(_("This does not appear to be a valid Indian mobile number"))
+        # Step 5: Check if number has already been claimed
+        existing = UserPhone.get(phone=number)
         if existing is not None:
             if existing.user == g.user:
                 raise wtforms.ValidationError(_("You have already registered this phone number"))
             else:
                 raise wtforms.ValidationError(_("This phone number has already been claimed"))
-        existing = UserPhoneClaim.get(phone=field.data, user=g.user)
+        existing = UserPhoneClaim.get(phone=number, user=g.user)
         if existing is not None:
             raise wtforms.ValidationError(_("This phone number is pending verification"))
-        # Step 1: Remove punctuation in number
-        field.data = strip_phone(field.data)
-        # Step 2: Validate number format
-        if not valid_phone(field.data):
-            raise wtforms.ValidationError(_("Invalid phone number (must be in international format with a leading + symbol)"))
-        # Step 3: Check if Indian number (startswith('+91'))
-        if not field.data.startswith('+91') or len(field.data) != 13:
-            raise wtforms.ValidationError(_("Only Indian mobile numbers are allowed at this time"))
+        field.data = number  # Save stripped number
 
 
 class VerifyPhoneForm(Form):
     verification_code = wtforms.TextField(__("Verification code"), validators=[wtforms.validators.Required()])
 
     def validate_verification_code(self, field):
+        # self.phoneclaim is set by the view before calling form.validate()
         if self.phoneclaim.verification_code != field.data:
             raise wtforms.ValidationError(_("Verification code does not match"))
