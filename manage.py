94ca4706ae64578a77e2b91ccc9026830559a861
Kiran Jonnalagadda <kiran@hasgeek.com>
2015-11-05 13:12:35 +0530

    Deprecate shell.py in favour of manage.py shell. Fixes #150

--- manage.py
+++ manage.py
@@ -2,11 +2,16 @@
 
 from coaster.manage import init_manager
 
+import lastuser_core
+import lastuser_oauth
+import lastuser_ui
+import lastuserapp
+import lastuser_core.models as models
 from lastuser_core.models import db
 from lastuserapp import app, init_for
 
 
 if __name__ == '__main__':
     db.init_app(app)
-    manager = init_manager(app, db, init_for)
+    manager = init_manager(app, db, init_for, lastuser_core=lastuser_core, lastuser_oauth=lastuser_oauth, lastuser_ui=lastuser_ui, lastuserapp=lastuserapp, models=models)
     manager.run()
