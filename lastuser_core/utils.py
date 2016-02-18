5346c6dbb8101a2582a38e32e39971b9c81f1a6d
Kiran Jonnalagadda <kiran@hasgeek.com>
2016-02-18 18:03:00 +0530

    Fix redirect URL construction when fragments are used.

--- lastuser_core/utils.py
+++ lastuser_core/utils.py
@@ -22,12 +22,11 @@ def make_redirect_url(url, use_fragment=False, **params):
     # 2: path
     # 3: query -- appended to
     # 4: fragment
-    queryparts = urlparse.parse_qsl(urlparts[3], keep_blank_values=True)
+    queryparts = urlparse.parse_qsl(urlparts[4] if use_fragment else urlparts[3], keep_blank_values=True)
     queryparts.extend([(k, v) for k, v in params.items() if v is not None])
     queryparts = [(key.encode('utf-8') if isinstance(key, unicode) else key,
                    value.encode('utf-8') if isinstance(value, unicode) else value) for key, value in queryparts]
     if use_fragment:
-        urlparts[3] = None
         urlparts[4] = make_query_string(queryparts)
     else:
         urlparts[3] = make_query_string(queryparts)
