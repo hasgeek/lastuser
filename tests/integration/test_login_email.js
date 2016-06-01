331d5ac6167483827ec848fcc780c13789860d28
Sindhu S <sindhus@live.in>
2016-06-01 12:13:48 +0530

    Remove helpers.js to simplify

--- tests/integration/test_login_email.js
+++ tests/integration/test_login_email.js
@@ -1,12 +1,25 @@
-var helpers = require('helpers.js'),
-    system = require('system'),
+var system = require('system'),
+    test_username = system.env.TEST_USERNAME,
+    test_password = system.env.TEST_PASSWORD,
     host = "http://localhost:7500";
 
 casper.test.begin('Lastuser redirects to correct Lastuser login page', 1, function (test){
     casper.start(host, function() {
         this.echo("Check for running server");
         test.assertHttpStatus(200, '200 OK');
-        helpers.login(casper, host);
+        casper.clear();
+        phantom.clearCookies();
+        casper.thenOpenAndEvaluate(host+"/login", function(test_username, test_password){
+            document.querySelector('#username').value = username;
+            document.querySelector('#password').value = password;
+            document.querySelector('#passwordlogin').submit();
+        }, test_username, test_password);
+        casper.waitForUrl(host, function(){
+            casper.thenOpen(host+"/apps", function(){
+                casper.echo("Logged in, indeed!");
+                casper.echo("Cookies:" + JSON.stringify(phantom.cookies));
+            });
+        });
     });
 
     casper.run(function(){
