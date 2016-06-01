5f9757a376288f090aa2eb507b3e5898e230849e
Sindhu S <sindhus@live.in>
2016-06-01 13:13:40 +0530

    Remove httpstatus code check in google login test

--- tests/integration/test_login_google.js
+++ tests/integration/test_login_google.js
@@ -1,7 +1,7 @@
 var system = require('system');
 var host = "http://localhost:7500";
 
-casper.test.begin('Lastuser redirects to correct Google login page', 3, function (test){
+casper.test.begin('Lastuser redirects to correct Google login page', 2, function (test){
     casper.start(host, function() {
         this.echo("Check for running server");
         test.assertHttpStatus(200, '200 OK');
@@ -10,7 +10,6 @@ casper.test.begin('Lastuser redirects to correct Google login page', 3, function
     login_url = host+"/login/google";
     casper.thenOpen(login_url, function() {
         this.echo("Hit login endpoint");
-        test.assertHttpStatus(200, "200 OK");
         this.echo("Check if redirected URL leads to Google");
         test.assertUrlMatch(/accounts.google.com\/o\/oauth2\/v2\/auth/, 'Redirected to Google login successfully');
     });
