hasgeek/lastuser

commit 9a95c158d47c1cbe7413565a4cb210adfa602ec5

Change source for user's org memberof list

lastuser_oauth/views/resource.py
@@ -38,7 +38,7 @@ def get_userinfo(user, client, scope=[], session=None, get_permissions=True):
     if '*' in scope or 'organizations' in scope or 'organizations/*' in scope:
         userinfo['organizations'] = {
             'owner': [{'userid': org.userid, 'name': org.name, 'title': org.title, 'domain': org.domain} for org in user.organizations_owned()],
-            'member': [{'userid': org.userid, 'name': org.name, 'title': org.title, 'domain': org.domain} for org in user.organizations()],
+            'member': [{'userid': org.userid, 'name': org.name, 'title': org.title, 'domain': org.domain} for org in user.organizations_memberof()],
             }
 
     if '*' in scope or 'organizations' in scope or 'teams' in scope or 'organizations/*' in scope or 'teams/*' in scope:

lastuser_core/models/user.py
@@ -257,14 +257,14 @@ class User(BaseMixin, db.Model):
 
     def organizations_memberof(self):
         """
-        Return the organizations this user is an owner of.
+        Return the organizations this user is a member of.
         """
         return sorted(set([team.org for team in self.teams if team.org.members == team]),
             key=lambda o: o.title)
 
     def organizations_memberof_ids(self):
         """
-        Return the database ids of the organizations this user is an owner of. This is used
+        Return the database ids of the organizations this user is a member of. This is used
         for database queries.
         """
         return list(set([team.org.id for team in self.teams if team.org.members == team]))

commit 8300dfd01b70d0aa2ca2bd04eb1c695a8ec6401e

Fix regression introduced by commit 9a95c158
It was no longer possible to correlate teams with organisations if the
user was not an owner or member. The (somewhat redundant) ‘all’ list
brings back those organisations.

lastuser_oauth/views/resource.py
@@ -39,6 +39,7 @@ def get_userinfo(user, client, scope=[], session=None, get_permissions=True):
         userinfo['organizations'] = {
             'owner': [{'userid': org.userid, 'name': org.name, 'title': org.title, 'domain': org.domain} for org in user.organizations_owned()],
             'member': [{'userid': org.userid, 'name': org.name, 'title': org.title, 'domain': org.domain} for org in user.organizations_memberof()],
+            'all': [{'userid': org.userid, 'name': org.name, 'title': org.title, 'domain': org.domain} for org in user.organizations()],
             }
 
     if '*' in scope or 'organizations' in scope or 'teams' in scope or 'organizations/*' in scope or 'teams/*' in scope: