6fee2e759cef29228d511aafeebf78d5d859931f
Karan Saini <37518297+qurbat@users.noreply.github.com>
2019-12-20 16:52:47 +0530

    Remove unused Gravatar functions (#375)
    * Remove Gravatar function from utils.py
    
    * Remove Gravatar function from test_utils.py

--- lastuser_core/utils.py
+++ lastuser_core/utils.py
@@ -46,26 +46,6 @@ def valid_phone(candidate):
     return not PHONE_VALID_RE.search(candidate) is None
 
 
-def get_gravatar_md5sum(url):
-    """
-    Retrieve the MD5 sum from a Gravatar URL. Returns None if the URL is invalid.
-
-    >>> get_gravatar_md5sum(
-    ...     'https://secure.gravatar.com/avatar/31b0e7df40a7e327e7908f61a314fe47?d=https'
-    ...     '://a248.e.akamai.net/assets.github.com%2Fimages%2Fgravatars%2Fgravatar-140.png')
-    '31b0e7df40a7e327e7908f61a314fe47'
-    """
-    parts = urlparse.urlparse(url)
-    if parts.netloc not in ['www.gravatar.com', 'secure.gravatar.com', 'gravatar.com']:
-        return None
-    if not parts.path.startswith('/avatar/'):
-        return None
-    md5sum = parts.path.split('/')[2]
-    if len(md5sum) != 32:
-        return None
-    return md5sum
-
-
 def mask_email(email):
     """
     Masks an email address
--- tests/unit/lastuser_core/test_utils.py
+++ tests/unit/lastuser_core/test_utils.py
@@ -3,7 +3,6 @@
 import unittest
 
 from lastuser_core.utils import (
-    get_gravatar_md5sum,
     make_redirect_url,
     mask_email,
     strip_phone,
@@ -29,47 +28,6 @@ class FlaskrTestCase(unittest.TestCase):
         assert valid_phone('+91 7676 33 2020') is False
         assert valid_phone('A91 7676 3A3 2020') is False
         assert valid_phone('*^$&766767**^&^') is False
-
-    def test_get_gravatar_md5sum(self):
-        """
-        Check if URL is gravatar URL and return extracted md5
-        """
-        self.assertIsNone(
-            get_gravatar_md5sum(
-                'https://secure.gravatar.com/a744b4c602f8fd32206eb44894259642'
-            )
-        )
-        self.assertIsNone(
-            get_gravatar_md5sum('gravatar.com/avatar/a744b4c602f8fd32206eb44894259642')
-        )
-        self.assertIsNone(
-            get_gravatar_md5sum(
-                'https://hasgeek.com/avatar/a744b4c602f8fd32206eb44894259642'
-            )
-        )
-        self.assertEqual(
-            get_gravatar_md5sum(
-                'https://secure.gravatar.com/avatar/a744b4c602f8fd32206eb44894259642'
-            ),
-            'a744b4c602f8fd32206eb44894259642',
-        )
-        self.assertEqual(
-            get_gravatar_md5sum(
-                'https://gravatar.com/avatar/a744b4c602f8fd32206eb44894259642'
-            ),
-            'a744b4c602f8fd32206eb44894259642',
-        )
-        self.assertNotEqual(
-            get_gravatar_md5sum(
-                'https://secure.gravatar.com/avatar/a744b4c602f8fd32206eb44894259642'
-            ),
-            'a744b4c602f8fd32206eb44894259641',
-        )
-        self.assertIsNone(
-            get_gravatar_md5sum(
-                'https://secure.gravatar.com/avatar/a744b4c602f8fd32206eb44894'
-            )
-        )
 
     def test_make_redirect_url(self):
         # scenario 1: straight forward splitting
