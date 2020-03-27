7c5f98547af0b04a5d69afa343ab2c49b26ba060
Kiran Jonnalagadda <kiran@hasgeek.com>
2020-03-27 11:34:33 +0530

    Remove unused notification code

--- lastuser_core/__init__.py
+++ lastuser_core/__init__.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
 
-from collections import OrderedDict
-
 from flask import Blueprint
 
 from .registry import LoginProviderRegistry, ResourceRegistry
@@ -11,7 +9,6 @@ lastuser_core = Blueprint('lastuser_core', __name__)
 #: Global resource registry
 resource_registry = ResourceRegistry()
 login_registry = LoginProviderRegistry()
-channel_registry = OrderedDict()
 
 # Register signals
 from . import signals  # NOQA  # isort:skip
